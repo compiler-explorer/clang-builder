7d12560359d6a3f9b1a60467aa4970267447e956
Austin Morton <apmorton@users.noreply.github.com>
2020-05-19 14:28:40 -0400

    Consolidate identical build scripts and avoid rebuilding the same revision for nightly builds (#11)

--- build/build.sh
+++ build/build.sh
@@ -2,53 +2,139 @@
 
 set -ex
 
-# Grab CE's GCC for its binutils
+ROOT=$PWD
+VERSION=$1
+
 BINUTILS_GCC_VERSION=9.2.0
+CMAKE_EXTRA_ARGS=
+LLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi"
+LLVM_EXPERIMENTAL_TARGETS_TO_BUILD=
+BASENAME=clang
+NINJA_TARGET=install
+
+case $VERSION in
+autonsdmi-trunk)
+    BRANCH=experiments
+    URL=https://github.com/cor3ntin/llvm-project.git
+    VERSION=autonsdmi-trunk-$(date +%Y%m%d)
+    CMAKE_EXTRA_ARGS=-DLLVM_OPTIMIZED_TABLEGEN=ON
+    ;;
+cppx-trunk)
+    BRANCH=compiler-explorer
+    URL=https://gitlab.com/lock3/clang.git
+    VERSION=cppx-trunk-$(date +%Y%m%d)
+    ;;
+embed-trunk)
+    BRANCH=feature/embed
+    URL=https://github.com/ThePhD/llvm-project.git
+    VERSION=embed-trunk-$(date +%Y%m%d)
+    ;;
+lifetime-trunk)
+    BRANCH=lifetime
+    URL=https://github.com/mgehre/llvm-project.git
+    VERSION=lifetime-trunk-$(date +%Y%m%d)
+    LLVM_ENABLE_PROJECTS="clang"
+    ;;
+relocatable-trunk)
+    BRANCH=trivially-relocatable
+    URL=https://github.com/Quuxplusone/llvm-project.git
+    VERSION=relocatable-trunk-$(date +%Y%m%d)
+    ;;
+llvm-*)
+    BASENAME=llvm
+    NINJA_TARGET=install-llvm-headers
+    # strip prefix from front of version
+    VERSION=${VERSION#llvm-}
+    if [[ "${VERSION}" == "trunk" ]]; then
+        BRANCH=master
+        VERSION=trunk-$(date +%Y%m%d)
+    else
+        TAG=llvmorg-${VERSION}
+    fi
+    URL=https://github.com/llvm/llvm-project.git
+    ;;
+*)
+    if [[ "${VERSION}" == "trunk" ]]; then
+        BRANCH=master
+        VERSION=trunk-$(date +%Y%m%d)
+    else
+        TAG=llvmorg-${VERSION}
+    fi
+    URL=https://github.com/llvm/llvm-project.git
+    LLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;compiler-rt;lld;polly;clang-tools-extra;openmp"
+    LLVM_EXPERIMENTAL_TARGETS_TO_BUILD="RISCV;WebAssembly"
+    ;;
+esac
+
+# use tag name as branch if otherwise unspecified
+BRANCH=${BRANCH-$TAG}
+
+# some builds checkout a tag instead of a branch
+# these builds have a different prefix for ls-remote
+REF=refs/heads/${BRANCH}
+if [[ ! -z "${TAG}" ]]; then
+    REF=refs/tags/${TAG}
+fi
+
+# determine build revision
+LLVMORG_REVISION=$(git ls-remote "${URL}" "${REF}" | cut -f 1)
+REVISION="llvmorg-${LLVMORG_REVISION}-gcc-${BINUTILS_GCC_VERSION}"
+LAST_REVISION="${3}"
+
+echo "ce-build-revision:${REVISION}"
+
+if [[ "${REVISION}" == "${LAST_REVISION}" ]]; then
+  echo "ce-build-status:SKIPPED"
+  exit
+fi
+
+# Grab CE's GCC for its binutils
 mkdir -p /opt/compiler-explorer
 pushd /opt/compiler-explorer
 curl -sL https://s3.amazonaws.com/compiler-explorer/opt/gcc-${BINUTILS_GCC_VERSION}.tar.xz | tar Jxf -
 popd
 
-ROOT=$(pwd)
-VERSION=$1
-if echo ${VERSION} | grep 'trunk'; then
-    TAG=master
-    VERSION=trunk-$(date +%Y%m%d)
-else
-    TAG=llvmorg-${VERSION}
-fi
-
-OUTPUT=/root/clang-${VERSION}.tar.xz
-S3OUTPUT=""
-if echo $2 | grep s3://; then
+FULLNAME=${BASENAME}-${VERSION}
+OUTPUT=${ROOT}/${FULLNAME}.tar.xz
+S3OUTPUT=
+if [[ $2 =~ ^s3:// ]]; then
     S3OUTPUT=$2
 else
-    OUTPUT=${2-/root/clang-${VERSION}.tar.xz}
+    if [[ -d "${2}" ]]; then
+        OUTPUT=$2/${FULLNAME}.tar.xz
+    else
+        OUTPUT=${2-$OUTPUT}
+    fi
 fi
 
-STAGING_DIR=$(pwd)/staging
-rm -rf ${STAGING_DIR}
-mkdir -p ${STAGING_DIR}
+BUILD_DIR=${ROOT}/build
+STAGING_DIR=${ROOT}/staging
+rm -rf "${STAGING_DIR}"
+mkdir -p "${STAGING_DIR}"
+mkdir -p "${ROOT}/build"
 
 # Setup llvm-project checkout
-git clone --depth 1 --single-branch -b "${TAG}" https://github.com/llvm/llvm-project.git
+git clone --depth 1 --single-branch -b "${BRANCH}" "${URL}" "${ROOT}/llvm-project"
 
 # Setup build directory and build configuration
-mkdir build
-cd build
-cmake -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi;compiler-rt;lld;polly;clang-tools-extra;openmp" -G "Ninja" ../llvm-project/llvm \
+mkdir -p "${ROOT}/build"
+cd "${ROOT}/build"
+cmake \
+    -G "Ninja" "${ROOT}/llvm-project/llvm" \
+    -DLLVM_ENABLE_PROJECTS="${LLVM_ENABLE_PROJECTS}" \
     -DCMAKE_BUILD_TYPE:STRING=Release \
-    -DCMAKE_INSTALL_PREFIX:PATH=/root/staging \
-    -DLLVM_BINUTILS_INCDIR:PATH=/opt/compiler-explorer/gcc-${BINUTILS_GCC_VERSION}/lib/gcc/x86_64-linux-gnu/${BINUTILS_GCC_VERSION}/plugin/include \
-    -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="RISCV;WebAssembly"
+    -DCMAKE_INSTALL_PREFIX:PATH="${STAGING_DIR}" \
+    -DLLVM_BINUTILS_INCDIR:PATH="/opt/compiler-explorer/gcc-${BINUTILS_GCC_VERSION}/lib/gcc/x86_64-linux-gnu/${BINUTILS_GCC_VERSION}/plugin/include" \
+    -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="${LLVM_EXPERIMENTAL_TARGETS_TO_BUILD}" \
+    ${CMAKE_EXTRA_ARGS}
 
 # Build and install artifacts
-ninja install
+ninja ${NINJA_TARGET}
 
 # Don't try to compress the binaries as they don't like it
 
 export XZ_DEFAULTS="-T 0"
-tar Jcf ${OUTPUT} --transform "s,^./,./clang-${VERSION}/," -C ${STAGING_DIR} .
+tar Jcf ${OUTPUT} --transform "s,^./,./${FULLNAME}/," -C ${STAGING_DIR} .
 
 if [[ ! -z "${S3OUTPUT}" ]]; then
     s3cmd put --rr ${OUTPUT} ${S3OUTPUT}
